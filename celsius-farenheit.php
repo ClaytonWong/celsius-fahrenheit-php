839b6333837c9c412a5190aadd31da41cefaefa9
Clayton <c_w_serious-github@yahoo.com.au>
2018-05-05 18:07:12 +1000

    Works for whole number inputs

--- celsius-farenheit.php
+++ celsius-farenheit.php
@@ -1,19 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Celsius to Farenheit converter</title>
+  <title>Celsius to Farenheit converter</title>
 </head>
 <body>
 <?php
 if(isset($_POST["number"])){
-    $x = $_POST["number"];
-    $x = $x * $x;
-    echo "<p>" . $_POST["number"] . " squared is " . $x . "</p>";
+  $c = $_POST["number"];
+
+  $f = ($c * 1.8) + 32;
+
+  if($c == 1)
+  {
+    echo "<p>" . $c . " degree Celsius =  " . $f . " degrees Farenheit" . "</p>";
+  }
+  elseif($f == 1)
+  {
+    echo "<p>" . $c . " degrees Celsius =  " . $f . " degree Farenheit" . "</p>";
+  }
+  else{
+    echo "<p>" . $c . " degrees Celsius =  " . $f . " degrees Farenheit" . "</p>";
+  }
 }
 ?>
 <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
-    <label for="inputNumber">number:</label>
-    <input type="number" name="number" id="inputNumber">
-    <input type="submit" value="Find square">
+  <label for="inputNumber">Temp. in Celsius:</label>
+  <input type="number" name="number" id="inputNumber">
+  <input type="submit" value="Find temp. in Farenheit">
 </form>
 </body>
