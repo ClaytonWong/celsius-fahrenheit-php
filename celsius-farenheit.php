a89095d5d3030e1aa0826a37bd74d729f78a4f85
Clayton <c_w_serious-github@yahoo.com.au>
2018-05-05 23:55:31 +1000

    Add validation for NULL input

--- celsius-farenheit.php
+++ celsius-farenheit.php
@@ -1,51 +1,73 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Celsius to Farenheit converter</title>
-</head>
-<body>
-<?php
-  if(isset($_POST["number"])){ // If input present in number field
-    if($_POST["conversion_type"] == "Celsius to Farenheit")
-    {
-      $c = $_POST["number"];
-
-      $f = ($c * 1.8) + 32;
-    
-      if($c == 1)
-      {
-        echo "<p>" . $c . " degree Celsius =  " . $f . " degrees Farenheit" . "</p>";
-      }
-      else
-      {
-        echo "<p>" . $c . " degrees Celsius =  " . $f . " degrees Farenheit" . "</p>";
-      }
-    }
-    else
-    {
-      $f = $_POST["number"];
-
-      $c = ($f - 32) / 1.8;
-
-      if($f == 1)
-      {
-        echo "<p>" . $f . " degree Farenheit = " . $c . " degrees Celsius" . "</p>";
-      }
-      else
-      {
-        echo "<p>" . $f . " degrees Farenheit = " . $c . " degrees Celsius" . "</p>";
-      }
-    }
-  }
-?>
-<form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+  <meta charset="UTF-8">
   
-  <input type="radio" name="conversion_type" value="Celsius to Farenheit" checked id="inputRadio">Celsius to Farenheit
-  <input type="radio" name="conversion_type" value="Farenheit to Celsius" id="inputRadio">Farenheit to Celsius
+  <title>
+    Celsius to Farenheit converter
+  </title>
 
-  <label for="inputNumber">Temp. in Celsius:</label>
-  <input type="number" step="any" name="number" id="inputNumber">
+  <style type="text/css">
+    .error{ color: red; }
+  </style>
+</head>
 
-  <input type="submit" value="Perform Conversion">
-</form>
+<body>
+
+  <?php
+    
+    if( isset($_POST["number"]) )
+    { 
+      $numberErr = "";
+
+      if( $_POST["number"] == NULL  )
+      {
+        $numberErr = "Please enter a floating point number";
+      }
+      else
+      {   
+        if($_POST["conversion_type"] == "Celsius to Farenheit")
+        {
+          $c = $_POST["number"];
+
+          $f = ($c * 1.8) + 32;
+        
+          if($c == 1 || $c == -1)
+          {
+            echo "<p>" . $c . " degree Celsius =  " . $f . " degrees Farenheit" . "</p>";
+          }
+          else
+          {
+            echo "<p>" . $c . " degrees Celsius =  " . $f . " degrees Farenheit" . "</p>";
+          }
+        }
+        else
+        {
+          $f = $_POST["number"];
+
+          $c = ($f - 32) / 1.8;
+
+          if($f == 1 || $f == -1)
+          {
+            echo "<p>" . $f . " degree Farenheit = " . $c . " degrees Celsius" . "</p>";
+          }
+          else
+          {
+            echo "<p>" . $f . " degrees Farenheit = " . $c . " degrees Celsius" . "</p>";
+          }
+        }
+      }
+    }
+  ?>
+  <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+    
+    <input type="radio" name="conversion_type" value="Celsius to Farenheit" checked id="inputRadio">Celsius to Farenheit
+    <input type="radio" name="conversion_type" value="Farenheit to Celsius" id="inputRadio">Farenheit to Celsius
+
+    <label for="inputNumber">Temp. to convert:</label>
+    <input type="number" step="any" name="number" id="inputNumber">
+    <span class="error"><?php echo $numberErr; ?></span>
+
+    <input type="submit" value="Perform Conversion">
+  </form>
 </body>
